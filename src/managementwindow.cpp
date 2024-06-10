03d4bbefd07876ba894fc4e0ee7b4426ce804d33
Adrian Gao <adrian.gao@outlook.com>
2024-06-10 23:10:46 +1000

    load html description for treasury futures

--- src/managementwindow.cpp
+++ src/managementwindow.cpp
@@ -1,14 +1,14 @@
 #include "brms/managementwindow.h"
 #include "ui_managementwindow.h"
+#include <QDebug>
+#include <QUrl>
 
 ManagementWindow::ManagementWindow(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::ManagementWindow)
-{
-    ui->setupUi(this);
+    : QWidget(parent), ui(new Ui::ManagementWindow) {
+  ui->setupUi(this);
+  auto src = QUrl::fromLocalFile(":/resources/treasury_futures.html");
+  ui->treasuryFuturesTextBrowser->setSource(src, QTextDocument::HtmlResource);
+  ui->treasuryFuturesTextBrowser->viewport()->setAutoFillBackground(false);
 }
 
-ManagementWindow::~ManagementWindow()
-{
-    delete ui;
-}
+ManagementWindow::~ManagementWindow() { delete ui; }
