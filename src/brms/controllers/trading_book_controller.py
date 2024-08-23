530fce3a373554c69bd5bcafb2d6c9552a90dd48
Adrian Gao <adrian.gao@outlook.com>
2024-08-23 17:42:12 +1000

    Model for producing data, not controller

--- src/brms/controllers/trading_book_controller.py
+++ src/brms/controllers/trading_book_controller.py
@@ -37,31 +37,8 @@ class TradingBookController:
 
     def update_assets_tree_view(self):
         headers = ["Asset (Long)", "Value"]
-        grouped_assets = defaultdict(lambda: defaultdict(list))
-
-        # Group assets by class type and name
-        for asset in self.model.assets:
-            grouped_assets[type(asset).instrument_type][asset.name].append(asset)
-
-        eval_date = ql.Settings.instance().evaluationDate
-
-        # Create the data structure for the tree view
-        data = []
-        # Each asset_type is like Cash, Loans, etc
-        for asset_type, assets_by_name in grouped_assets.items():
-            asset_type_val = 0.0
-            asset_type_data = {"data": [asset_type, asset_type_val], "children": []}
-            # Each asset within a type represents a specific asset
-            for name, assets in assets_by_name.items():
-                total_value = sum(a.value_on_trading_book(eval_date) for a in assets)
-                asset_type_val += total_value
-                asset_type_data["children"].append({"data": [name, total_value]})
-
-            asset_type_data["data"] = [asset_type, asset_type_val]
-            data.append(asset_type_data)
-
+        data = self.model.assets_data()
         self.view.assets_tree_view.setModel(TreeModel(headers, data))
-
         # fmt: off
         self.view.assets_tree_view.header().setSectionResizeMode(0, QHeaderView.Stretch)
         self.view.assets_tree_view.header().setSectionResizeMode(1, QHeaderView.ResizeToContents)
@@ -70,35 +47,8 @@ class TradingBookController:
 
     def update_liabilities_tree_view(self):
         headers = ["Liability (Short)", "Value"]
-        grouped_liabilities = defaultdict(lambda: defaultdict(list))
-
-        # Group liabilities by class type and name
-        # fmt: off
-        for liability in self.model.liabilities:
-            grouped_liabilities[type(liability).instrument_type][liability.name].append(liability)
-
-        eval_date = ql.Settings.instance().evaluationDate
-
-        # Create the data structure for the tree view
-        data = []
-        # Each liability_type is like Loans, Mortgages, etc
-        for liability_type, liabilities_by_name in grouped_liabilities.items():
-            liability_type_val = 0.0
-            liability_type_data = {
-                "data": [liability_type, liability_type_val],
-                "children": [],
-            }
-            # Each liability within a type represents a specific liability
-            for name, liabilities in liabilities_by_name.items():
-                total_value = sum(l.value_on_trading_book(eval_date) for l in liabilities)
-                liability_type_val += total_value
-                liability_type_data["children"].append({"data": [name, total_value]})
-
-            liability_type_data["data"] = [liability_type, liability_type_val]
-            data.append(liability_type_data)
-
+        data = self.model.liabilities_data()
         self.view.liabilities_tree_view.setModel(TreeModel(headers, data))
-
         # fmt: off
         self.view.liabilities_tree_view.header().setSectionResizeMode(0, QHeaderView.Stretch)
         self.view.liabilities_tree_view.header().setSectionResizeMode(1, QHeaderView.ResizeToContents)
--- src/brms/models/bank_banking_book_model.py
+++ src/brms/models/bank_banking_book_model.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+
+import QuantLib as ql
+
 from .bank_book_model import BankBookModel
 
 
@@ -5,3 +9,60 @@ class BankBankingBookModel(BankBookModel):
 
     def __init__(self) -> None:
         super().__init__()
+
+    def assets_data(self):
+
+        grouped_assets = defaultdict(lambda: defaultdict(list))
+        # Group assets by class type and name
+        for asset in self.assets:
+            grouped_assets[type(asset).instrument_type][asset.name].append(asset)
+
+        eval_date = ql.Settings.instance().evaluationDate
+
+        # Create the data structure for the tree view
+        data = []
+        # Each asset_type is like Cash, Loans, etc
+        for asset_type, assets_by_name in grouped_assets.items():
+            asset_type_val = 0.0
+            asset_type_data = {"data": [asset_type, asset_type_val], "children": []}
+            # Each asset within a type represents a specific asset
+            for name, assets in assets_by_name.items():
+                total_value = sum(a.value_on_banking_book(eval_date) for a in assets)
+                asset_type_val += total_value
+                asset_type_data["children"].append({"data": [name, total_value]})
+
+            asset_type_data["data"] = [asset_type, asset_type_val]
+            data.append(asset_type_data)
+
+        return data
+
+    def liabilities_data(self):
+
+        grouped_liabilities = defaultdict(lambda: defaultdict(list))
+
+        # Group liabilities by class type and name
+        # fmt: off
+        for liability in self.liabilities:
+            grouped_liabilities[type(liability).instrument_type][liability.name].append(liability)
+
+        eval_date = ql.Settings.instance().evaluationDate
+
+        # Create the data structure for the tree view
+        data = []
+        # Each liability_type is like Loans, Mortgages, etc
+        for liability_type, liabilities_by_name in grouped_liabilities.items():
+            liability_type_val = 0.0
+            liability_type_data = {
+                "data": [liability_type, liability_type_val],
+                "children": [],
+            }
+            # Each liability within a type represents a specific liability
+            for name, liabilities in liabilities_by_name.items():
+                total_value = sum(l.value_on_banking_book(eval_date) for l in liabilities)
+                liability_type_val += total_value
+                liability_type_data["children"].append({"data": [name, total_value]})
+
+            liability_type_data["data"] = [liability_type, liability_type_val]
+            data.append(liability_type_data)
+
+        return data
