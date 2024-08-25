3720789b1d7ffe36d3fa0391745800eb4b857573
Adrian Gao <adrian.gao@outlook.com>
2024-08-25 17:37:38 +1000

    Update about

--- src/brms/__init__.py
+++ src/brms/__init__.py
@@ -7,21 +7,20 @@ __github__ = "https://github.com/mgao6767/brms"
 __about__ = f"""
 <p><strong>BRMS - Bank Risk Management Simulation</strong> v{__version__}</p>
 
-<p>This program is developed by Dr. <a href="https://mingze-gao.com">
-Mingze Gao</a> from Macquarie University.</p>
+<p>BRMS is developed by Dr. <a href="https://mingze-gao.com">Mingze Gao</a> 
+from the Department of Applied Finance at 
+<a href="https://www.mq.edu.au/macquarie-business-school">
+Macquarie Business School</a>.</p>
 
 <p>BRMS is an educational tool designed to provide users with an in-depth 
 understanding of bank risk management practices. It allows users to 
 simulate and respond to various risk scenarios, and analyze the impact on 
 a bank's financial health.</p>
 
-<p>This application is intended for educational purposes only. It should 
-not be used as a substitute for professional risk management advice.</p>
+<p>This application is for educational purposes only and not a substitute 
+for professional risk management advice. No liability is assumed for its use. 
+BRMS is licensed under the MIT License.</p>
 
-<p>No liability or warranty of any kind is assumed in the use of this 
-application. For further information, please contact the developer at 
+<p>For more information, please contact the developer at 
 <a href="mailto:mingze.gao@mq.edu.au">mingze.gao@mq.edu.au</a>.</p>
-
-<p>This project is licensed under the MIT License. For more details, please 
-refer to the LICENSE file included with the project.</p>
 """
