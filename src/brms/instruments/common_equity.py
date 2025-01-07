588d120914a951ae088fc4c568e91c5aaef28e4a
Adrian Gao <adrian.gao@outlook.com>
2025-01-07 21:52:31 +1100

    Remove unnecessary constructor from CommonEquity class

--- src/brms/instruments/common_equity.py
+++ src/brms/instruments/common_equity.py
@@ -8,10 +8,6 @@ from brms.models.scenario import Scenario
 class CommonEquity(Instrument):
     """A class to represent common equity instruments."""
 
-    def __init__(self, name: str, parent: Instrument | None = None) -> None:
-        """Initialize a CommonEquity instance."""
-        super().__init__(name, parent)
-
     def accept(self, visitor: ValuationVisitor, scenario: Scenario) -> float:
         """Accept a valuation visitor to calculate the instrument's value."""
         return visitor.value_common_equity(self, scenario)
