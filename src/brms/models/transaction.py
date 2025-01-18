7e03bdc81f98f0557fab477ebc3aca39c5c277e0
Adrian Gao <adrian.gao@outlook.com>
2025-01-18 21:21:14 +1100

    Add LoanRepaymentTransaction class to handle loan repayment transactions

--- src/brms/models/transaction.py
+++ src/brms/models/transaction.py
@@ -326,6 +326,57 @@ class LoanDisbursementTransaction(Transaction):
         )
 
 
+class LoanRepaymentTransaction(Transaction):
+    """Class representing a matured loan repayment transaction."""
+
+    def __init__(
+        self,
+        bank: Bank,
+        instrument: Instrument,  # TODO: specify all instrument types that can be a loan?
+        date: datetime.date | None = None,
+        description: str = "",
+    ) -> None:
+        self.cash_to_receive = Cash(value=instrument.value)
+        super().__init__(
+            bank=bank,
+            instrument=instrument,
+            value=instrument.value,
+            transaction_type=TransactionType.LOAN_REPAYMENT,
+            transaction_date=date,
+            description=description,
+        )
+
+    def execute(self) -> None:
+        self.bank.banking_book.remove_instrument(self.instrument, Position.LONG)
+        self.bank.banking_book.add_instrument(self.cash_to_receive, Position.LONG)
+        self.bank.ledger.post(self.journal_entry)
+
+    def undo(self) -> None:
+        self.bank.banking_book.add_instrument(self.instrument, Position.LONG)
+        self.bank.banking_book.remove_instrument(self.cash_to_receive, Position.LONG)
+        self.bank.ledger.post(self.reverse_journal_entry)
+
+    @property
+    def journal_entry(self) -> JournalEntry:
+        return SimpleEntry(
+            debit_account=self.bank.chart_of_accounts.cash_account,
+            credit_account=self.bank.chart_of_accounts.loan_account,
+            value=self.value,
+            date=self.transaction_date,
+            description=self.description,
+        )
+
+    @property
+    def reverse_journal_entry(self) -> JournalEntry:
+        return SimpleEntry(
+            debit_account=self.bank.chart_of_accounts.loan_account,
+            credit_account=self.bank.chart_of_accounts.cash_account,
+            value=self.value,
+            date=self.transaction_date,
+            description=self.description,
+        )
+
+
 if __name__ == "__main__":
     from brms.accounting.account import AccountBalances
     from brms.accounting.report import Report
