f890347cd141032b3af5fb08c946b2cc475774bb
Adrian Gao <adrian.gao@outlook.com>
2025-01-22 12:08:38 +1100

    Remove unused test method from MainController to clean up code

--- src/brms/controllers/main_controller.py
+++ src/brms/controllers/main_controller.py
@@ -24,7 +24,6 @@ class MainController(BRMSController):
         )
         # Connect signals
         self.connect_signals()
-        self._test()
 
     def connect_signals(self) -> None:
         """Connect signals from the view to the controller's slots."""
@@ -34,42 +33,3 @@ class MainController(BRMSController):
         """Handle the exit signal from the view."""
         # Perform any cleanup or save operations here
         self.view.close()
-
-    def _test(self) -> None:
-        import QuantLib as ql
-        from brms.instruments.mock import MockInstrument
-        from brms.models.bank_book import Position
-        from brms.models.base import BookType
-        from brms.instruments.base import BookType, CreditRating, Issuer, IssuerType
-        from brms.instruments.fixed_rate_bond import FixedRateBond
-
-        for i in range(100):
-            instrument_name = f"mock_instrument_{i}"
-            self.bank_ctrl.add_instrument_to_banking_book(MockInstrument(instrument_name), position=Position.LONG)
-            self.bank_ctrl.add_instrument_to_banking_book(MockInstrument(instrument_name), position=Position.SHORT)
-
-        for i in range(100):
-            instrument_name = f"mock_instrument_{i}"
-            inst = MockInstrument(instrument_name, book_type=BookType.TRADING_BOOK)
-            inst.value = i * 100
-            self.bank_ctrl.add_instrument_to_trading_book(inst, position=Position.LONG)
-
-        face_value = 1000.0
-        coupon_rate = 0.05
-        issue_date = ql.Date(1, 1, 2020)
-        maturity_date = ql.Date(1, 1, 2030)
-        bond = FixedRateBond(
-            face_value=face_value,
-            coupon_rate=coupon_rate,
-            issue_date=issue_date,
-            maturity_date=maturity_date,
-            book_type=BookType.TRADING_BOOK,
-            credit_rating=CreditRating.AA_MINUS,
-            issuer=Issuer(
-                name="Asian Development Bank",
-                issuer_type=IssuerType.MDB,
-                credit_rating=CreditRating.AA,
-            ),
-        )
-
-        self.bank_ctrl.add_instrument_to_banking_book(bond, position=Position.LONG)
