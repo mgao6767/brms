da7b667126a4cd61e69e06dcbdd1bd545cc88b26
Adrian Gao <adrian.gao@outlook.com>
2025-01-29 08:22:42 +1100

    Refactor create_bank_init_transactions to use generator for transaction creation; update simulation dates and streamline transaction handling

--- src/brms/data/default/__init__.py
+++ src/brms/data/default/__init__.py
@@ -1,5 +1,6 @@
 import datetime
 import random
+from collections.abc import Generator
 
 from dateutil.relativedelta import relativedelta
 
@@ -7,37 +8,43 @@ from brms.instruments.base import InstrumentClass
 from brms.instruments.factory import InstrumentFactory
 from brms.instruments.visitors.valuation import BankingBookValuationVisitor
 from brms.models.bank import Bank
+from brms.models.bank_engine import BankEngine
 from brms.models.scenario import ScenarioManager
 from brms.models.transaction import Transaction, TransactionFactory, TransactionType
 
 random.seed(42)
 
-base_date = datetime.date(2021, 10, 21)
 
+# The oddest date of transactions
+SIMULATION_BASE_DATE = datetime.date(2020, 10, 1)
+# The start date of simulation
 SIMULATION_START_DATE = datetime.date(2022, 1, 3)
 
+base_date = datetime.date(2021, 10, 21)
+mortgage_issue_date = SIMULATION_BASE_DATE
 
-def create_bank_init_transactions(bank: Bank, scenario_manager: ScenarioManager) -> list[Transaction]:
+
+def create_bank_init_transactions(bank: Bank, scenario_manager: ScenarioManager) -> Generator[Transaction, None, None]:
     """Create a default list of transactions that initializes a bank."""
-    transactions = [
-        TransactionFactory.create_transaction(
-            bank=bank,
-            transaction_type=TransactionType.EQUITY_ISSUANCE,
-            instrument=InstrumentFactory.create_common_equity(value=1_000_000),
-            transaction_date=base_date,
-            description="Shareholders' contribution",
-        ),
-        TransactionFactory.create_transaction(
-            bank=bank,
-            transaction_type=TransactionType.DEPOSIT_RECEIVED,
-            instrument=InstrumentFactory.create_deposit(value=6_000_000),
-            transaction_date=base_date + relativedelta(days=2),
-            description="Deposits",
-        ),
-    ]
+
+    yield TransactionFactory.create_transaction(
+        bank=bank,
+        transaction_type=TransactionType.EQUITY_ISSUANCE,
+        instrument=InstrumentFactory.create_common_equity(value=1_000_000),
+        transaction_date=base_date,
+        description="Shareholders' contribution",
+    )
+
+    yield TransactionFactory.create_transaction(
+        bank=bank,
+        transaction_type=TransactionType.DEPOSIT_RECEIVED,
+        instrument=InstrumentFactory.create_deposit(value=6_000_000),
+        transaction_date=base_date + datetime.timedelta(days=2),
+        description="Deposits",
+    )
 
     # HTM banking book security, a Treasury Note
-    today = base_date + relativedelta(days=4)
+    today = base_date + datetime.timedelta(days=4)
     assert scenario_manager.has_scenario(today)
 
     tn = InstrumentFactory.create_treasury_note(
@@ -48,18 +55,32 @@ def create_bank_init_transactions(bank: Bank, scenario_manager: ScenarioManager)
         instrument_class=InstrumentClass.HTM,
     )
     tn.accept(BankingBookValuationVisitor(scenario_manager, valuation_date=today))
-    tx = TransactionFactory.create_transaction(
+    yield TransactionFactory.create_transaction(
         bank=bank,
         transaction_type=TransactionType.SECURITY_PURCHASE_HTM,
         instrument=tn,
         transaction_date=today,
         description="Purchase banking book security HTM",
     )
-    transactions.append(tx)
+
+    # Mortgage loan
+    mortgage = InstrumentFactory.create_residential_mortgage(
+        face_value=1_000_000,
+        interest_rate=0.0618,
+        issue_date=mortgage_issue_date,
+        maturity_years=2,
+    )
+    tx = TransactionFactory.create_transaction(
+        bank=bank,
+        transaction_type=TransactionType.LOAN_DISBURSEMENT,
+        instrument=mortgage,
+        transaction_date=mortgage_issue_date,
+        description="Issue a residential mortgage loan",
+    )
+    yield tx
 
     # FVOCI banking book security, a Treasury Note
     fvoci_securities = []
-
     for i in range(10):
         tn_fvoci = InstrumentFactory.create_treasury_note(
             face_value=100_000.0,
@@ -77,37 +98,14 @@ def create_bank_init_transactions(bank: Bank, scenario_manager: ScenarioManager)
             description="Purchase banking book security FVOCI",
         )
         fvoci_securities.append(tn_fvoci)
-        transactions.append(tx)
+        yield tx
 
     # Marking to market
-    current_date = today + relativedelta(days=1)
-    end_date = scenario_manager.current_scenario.date
+    current_date = SIMULATION_BASE_DATE
+    end_date = SIMULATION_START_DATE
 
-    visitor = BankingBookValuationVisitor(scenario_manager)
+    engine = BankEngine(bank, scenario_manager)
+    # day by day
     while current_date <= end_date:
-        if scenario_manager.has_scenario(current_date):
-            matured = [sec for sec in fvoci_securities if sec.maturity_date <= current_date]
-            for sec in matured:
-                tx = TransactionFactory.create_transaction(
-                    bank=bank,
-                    transaction_type=TransactionType.SECURITY_SALE_FVOCI,
-                    instrument=sec,
-                    transaction_date=current_date,  # or last day before maturity
-                    description="Sell banking book security FVOCI",
-                )
-                transactions.append(tx)
-
-            fvoci_securities = [sec for sec in fvoci_securities if sec not in matured]
-            for fvoci_security in fvoci_securities:
-                tx = TransactionFactory.create_transaction(
-                    bank=bank,
-                    transaction_type=TransactionType.SECURITY_FVOCI_MARK_TO_MARKET,
-                    instrument=fvoci_security,
-                    transaction_date=current_date,
-                    valuation_visitor=visitor,
-                    description="Mark to market banking book security FVOCI",
-                )
-                transactions.append(tx)
-        current_date += relativedelta(days=1)
-
-    return transactions
+        yield from engine.generate_transactions(current_date)
+        current_date += datetime.timedelta(days=1)
