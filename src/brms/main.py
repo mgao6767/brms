5d33a76dc71e28561807a0a2d00d3ad158c7640d
Adrian Gao <adrian.gao@outlook.com>
2026-04-05 07:43:29 +1000

    feat: rewire main.py to use core services

--- src/brms/main.py
+++ src/brms/main.py
@@ -6,8 +6,42 @@ from PySide6.QtWidgets import QApplication
 
 from brms import DEBUG_MODE
 from brms.controllers.main_controller import MainController
+from brms.core.events import EventBus
+from brms.core.metrics.base import MetricRegistry
+from brms.core.models.accounting.rules.base import RuleRegistry
+from brms.core.models.accounting.service import AccountingService
+from brms.core.models.history import SimulationHistory
+from brms.core.services.metrics_service import MetricsService
+from brms.core.services.risk_service import RiskService
+from brms.core.services.valuation_service import ValuationService
 from brms.models.simulation import Simulation as SimulationModel
 from brms.views.main_window import MainWindow
+
+
+def _build_core_services() -> dict:
+    """Instantiate and wire core domain services.
+
+    Returns a dict of named services that can be passed to controllers.
+    """
+    event_bus = EventBus()
+    rule_registry = RuleRegistry()
+    metric_registry = MetricRegistry()
+    accounting_service = AccountingService()
+    metrics_service = MetricsService(metric_registry)
+    valuation_service = ValuationService()
+    risk_service = RiskService()
+    history = SimulationHistory()
+
+    return {
+        "event_bus": event_bus,
+        "rule_registry": rule_registry,
+        "metric_registry": metric_registry,
+        "accounting_service": accounting_service,
+        "metrics_service": metrics_service,
+        "valuation_service": valuation_service,
+        "risk_service": risk_service,
+        "history": history,
+    }
 
 
 class App(QApplication):
@@ -19,9 +53,19 @@ class App(QApplication):
         font = self.font()
         font.setFamily("Monospace")
         self.setFont(font)
-        self.view = MainWindow()
+
+        # Core domain services (new architecture)
+        self.core_services = _build_core_services()
+
+        # Legacy simulation model (still used by controllers and views)
         self.model = SimulationModel()
-        self.controller = MainController(self.model, self.view)
+
+        self.view = MainWindow()
+        self.controller = MainController(
+            self.model,
+            self.view,
+            core_services=self.core_services,
+        )
         self.view.show()
         if DEBUG_MODE:
             self.view.debug_panel.show()
